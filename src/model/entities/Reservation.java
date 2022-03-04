d3423e7efa0b34da7aba4571f90000f38f89933c
Ysmar Victor <ysmarvictor@gmail.com>
2022-03-04 02:04:20 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer rooNumber;
@@ -13,6 +15,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer rooNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)){
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.rooNumber = rooNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -41,19 +46,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
+		// IllegalArgumentException -> Argumentos passados são inválidos
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)){
-			return"Check-out date must be after check-in date";
+			throw new DomainException("Check-out date must be after check-in date");
 		}
-
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		// Retornar nulo para sinalizar que não teve nenhum tipo de erro
-		return null;
 	}
 
 	@Override
